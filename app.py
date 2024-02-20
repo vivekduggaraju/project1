d41653e30460314c7a96f9efbe79b28a5fb705df
vivekduggaraju <105599859+vivekduggaraju@users.noreply.github.com>
2024-02-20 22:32:23 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-  return "hello world"
-
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug="True")
+  print("h11qesadlkfjld")
+  return render_template('home.html')
 
 
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
